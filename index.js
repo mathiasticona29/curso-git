2e92f2ef35ecefd0548d7b6281b2787005fc6ef3
mathias.29 <mathias.ticona29@gmail.com>
2026-02-07 15:23:21 -0500

    Render posts

--- index.js
+++ index.js
@@ -16,15 +16,28 @@ const getPosts = async (user) => {
 
 const getCommentsForEachPost = async (posts) => {
   const res = await Promise.all(posts.map(post => 
-    fetch(`${url}/comments?postId=${post.id}&_limit=4`)
-  ));
-  console.log(res); 
+    fetch(`${url}/comments?postId=${post.id}&_limit=2`)  
+  ))
   const postComments = await Promise.all(res.map(r => r.json()));
   
   postComments.forEach((comments, i) => posts[i].comments = comments);
 }
 
-
+const renderHtml = (user, posts) => {
+    const content = document.getElementById('content');
+    content.innerHTML += `<h3>Posts del usuario ${user.email}</h3>`;
+    
+    posts.forEach(post => {
+      content.innerHTML += `
+      <div class="post">
+        <h4>${post.title}</h4>
+        <p>${post.body}</p>
+        <br>
+        ${post.comments.map(c => `<p><span>${c.email}:</span>${c.body}</p>`).join('')}
+      </div>
+      `;
+    })
+}
 
 const getBlogContent = async () => {
   try {
@@ -32,29 +45,10 @@ const getBlogContent = async () => {
     const posts = await getPosts(user);
     await getCommentsForEachPost(posts);
 
-    console.log(user);
-    console.log(posts);
+    renderHtml(user, posts);
   } catch (err) {
     console.log(err);
   }
 }
 
 getBlogContent();
-
-const loadAdds = () => {
-  console.log('Adds loaded');
-}
-
-const affiliateRedirect = () => {
-  // resolver el bug
-}
-
-// test commit
-
-// cambio 1
-// cambio 2
-// cambio 3
-
-// cambios videos 1
-// cambios videos 2
-// cambios videos 3
